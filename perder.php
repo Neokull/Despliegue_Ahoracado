874b6c29fc84e584768892a52724595f57be12ed
Manuel Granero <magrfe01@alumnatflorida.es>
2025-11-04 15:24:43 +0100

    fix title in perder.php

--- perder.php
+++ perder.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Has ganado</title>
+    <title>Has perdido</title>
 </head>
 <body>
     <p>"Lo siento has perdido"</p>
